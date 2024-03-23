dc81b168929ea1742f3fb1ec8eb9c105d7a2e6bc
Tuan Huy <huydo.01012003@gmail.com>
2024-03-23 11:57:37 +0700

    fix conflict

--- src/route/favouriteProductRoute.js
+++ src/route/favouriteProductRoute.js
@@ -6,16 +6,9 @@ const {verifyAccessToken} = require('../middlewareControllers/verifyAccessTokenM
 const favouriteProductRoute = express.Router()
 // GET: /api/v1/favourite
 favouriteProductRoute.get('/', verifyAccessToken, favouriteProductController.getAllFavouriteProduct)
-<<<<<<< HEAD:src/route/favouriteProductRoute.js
 // GET: /api/v1/favourite/limit
 favouriteProductRoute.get('/limit', verifyAccessToken, favouriteProductController.getAllFavouriteProductLimit)
 // POST: /api/v1/favourite/create
 favouriteProductRoute.post('/create',  verifyAccessToken, favouriteProductController.changeProductFavourite)
-=======
-// GET: /api/v1/favourite
-favouriteProductRoute.get('/limit', verifyAccessToken, favouriteProductController.getAllFavouriteProductLimit)
-
-favouriteProductRoute.post('/api/add-product-favourite',  verifyAccessToken, favouriteProductController.changeProductFavourite)
->>>>>>> 89e38582dad20c4182ab0beb0a93397c5078df64:src/route/favouriteRoute.js
 
 module.exports = favouriteProductRoute
